19791356dd50c99be4cb749180dd783f64f99043
Samuel Theophilus <samtheo1597@outlook.com>
2022-08-01 15:07:16 +0100

    Add Custom Items and processors

--- web_scraping/scrapy101/items.py
+++ web_scraping/scrapy101/items.py
@@ -3,10 +3,51 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from datetime import datetime
+from scrapy import Item, Field
+from scrapy.loader.processors import MapCompose, TakeFirst
+from datetime import datetime
 
 
-class Scrapy101Item(scrapy.Item):
+def remove_quotes(text):
+    text=text.strip(u'\u201c'u'\u201d')
+    return text
+
+def convert_date(text):
+    return datetime.strptime(text,'%B %d, %Y')
+
+def parse_location(text):
+    return text[3:]
+
+class Scrapy101Item(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    quote_content=Field(
+        input_processor=MapCompose(remove_quotes),
+        output_processor=TakeFirst()
+        )
+
+    tags=Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+
+    author_name=Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+
+    author_birthday=Field(
+        input_processor=MapCompose(convert_date),
+        output_processor=TakeFirst()
+    )
+
+    author_bornlocation=Field(
+        input_processor=MapCompose(parse_location),
+        output_processor=TakeFirst()
+    )
+
+    author_bio=Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
--- web_scraping/scrapy101/spiders/quotes-spider.py
+++ web_scraping/scrapy101/spiders/quotes-spider.py
@@ -1,4 +1,6 @@
 import scrapy as sp
+from scrapy.loader import ItemLoader
+from scrapy101.items import Scrapy101Item
 
 class QuotesSpider(sp.Spider):
     name='quotes'
@@ -12,11 +14,25 @@ class QuotesSpider(sp.Spider):
         quotes= response.css('div .quote')
 
         for quote in quotes:
-            yield {
+            '''yield {
                 'text': quote.css('.text::text').get(),
                 'author':quote.css('.author::text').get(),
                 'tags':quote.css('.tag::text').getall(),
-            }
+
+            }'''
+
+            loader=ItemLoader(item=Scrapy101Item(), selector=quote)
+
+            loader.add_css('quote_content','.text::text')
+            loader.add_css('tags','.tag::text')
+
+            quote_item=loader.load_item()
+
+            #get author details
+            author_url= quote.css('.author + a::attr(href)').get()
+            self.logger.info('getting Author page URL')
+
+            yield response.follow(author_url, callback=self.parse_author,meta={'quote_item':quote_item})
 
         next_page=response.css('li.next a::attr(href)').get()
 
@@ -24,3 +40,20 @@ class QuotesSpider(sp.Spider):
             next_page=response.urljoin(next_page)
 
             yield sp.Request(next_page, callback=self.parse)
+    
+
+    def parse_author(self, response):
+        '''yield {
+            'author_name':response.css('.author-title::text').get(),
+            'author_birthday':response.css('.author-born-date::text').get(),
+            'author_bornlocation': response.css('.author-born-location::text').get(),
+            'author_bio':response.css('author-description::text').get(),
+        }'''
+
+        quote_item=response.meta['quote_item']
+        loader=ItemLoader(item=quote_item, response=response)
+        loader.add_css('author_name','.author-title::text')
+        loader.add_css('author_birthday','.author-born-date::text')
+        loader.add_css('author_bornlocation','.author-born-location::text')
+        loader.add_css('author_bio','.author-description::text')
+        yield loader.load_item()
